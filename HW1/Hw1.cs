1d3e71f0befe7d88e0680b7ffc3e518c270c5962
poliq-663 <polina.baranova.00@bk.ru>
2023-03-11 20:49:02 +0300

    Homework1 CSharp

---
+++ HW1/Hw1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        string? text = Console.ReadLine();
+        Console.WriteLine( text +", привет!");
+    }
+}
